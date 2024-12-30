62604196f1fc2cedff939ad305517e8be3926f62
m_1337 <eng.medhathassanali@gmail.com>
2024-12-30 10:05:58 -0500

    Update Main with test scenario

--- src/main.cpp
+++ src/main.cpp
@@ -7,47 +7,50 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-    double speed = 1000;
-    double averageServiceTime = 42.0;
+    double speed = 10; // Control the speed of the clock (eq -> simulation time = actual time * speed)
+    double averageServiceTime = 40.0; // Control the average service time of the 
+    int NumberofServers = 3; // Conrtol number of servers used
 
-    GlobalClock clock(speed);
-    LoadBalancer LB;
-    TaskGenerator TG(averageServiceTime, "task_log.txt", &clock);
+    GlobalClock clock(speed);// Create clock instance with the controled speed
+    LoadBalancer LB;// Create load balacer server instance
+    TaskGenerator TG(averageServiceTime, "task_log.txt", &clock);// Create task genrator server instance (average service time - log file - clock reference)
 
-    std::vector<std::shared_ptr<ServerQueue>> servers;
-    for (int i = 0; i < 10; ++i) {
-        servers.push_back(std::make_shared<ServerQueue>(
-            i + 1, 50.0 + i * 5, 20 + i * 5, &clock,
-            [&LB](std::pair<int, double> utilizationData) {
+    vector<shared_ptr<ServerQueue>> servers;// Create a vector of server instance .
+    for (int i = 0; i < NumberofServers; ++i) {
+    // (service id - server processing power - qeue size - clock reference - utilization call back function)
+        servers.push_back(make_shared<ServerQueue>(
+            i + 1, 15.0 + i * 5, 10 + i * 5, &clock,
+            [&LB](pair<int, double> utilizationData) {
                 LB.trackUtil(utilizationData.first, utilizationData.second);
             }));
     }
     
-    LB.setServers(servers);
+    LB.setServers(servers);//Conect all the servers to the load balacer
     
-    TG.start(1.0, [&LB](std::pair<int, double> task) {
+    //(Task generate frequency (inter arrival time)- task call back function)
+    TG.start(0.3, [&LB](pair<int, double> task) {
         Task tasklb = {task.first, task.second};
         LB.sendTask(tasklb);
     });
     
-    double simulationDuration = 5 * 3600;
+    double simulationDuration = 2 * 3600;//Set a simulation duration for 2h 
     while (clock.getCurrentTime() < simulationDuration) {
-        this_thread::sleep_for(chrono::milliseconds(100));
+        this_thread::sleep_for(chrono::milliseconds(100));// Avoid busy waiting
     }
 
-    TG.stop();
+    TG.stop(); //Manual stop task genrator server 
 
-    for (int i = 0; i < 10; ++i) {
-        servers[i]->calculateAverageWaitTime();
-        servers[i]->calculateAverageQueueOccupancy();
-        servers[i]->stopProcessing(); // Manual stop to avoid destructor being called automatically
+    for (int i = 0; i < NumberofServers; ++i) {
+        servers[i]->calculateAverageWaitTime();// calculate average waitTime for each server
+        servers[i]->calculateAverageQueueOccupancy();// calculate average queue occupancy for each server
+        servers[i]->stopProcessing(); // Manual stop each server to avoid destructor being called automatically
     }
 
     vector<string> logFiles;
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < NumberofServers; ++i) {
         logFiles.push_back("server" + to_string(i + 1) + "_log.txt");
     }
-    analyzer(logFiles, "task_log.txt");
+    analyzer(logFiles, "task_log.txt");// Analyze the simulation results
 
     return 0;
 }
